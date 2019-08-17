b947e8785d9476c46c88588124a55c68fcb293c2
Cody ENgel <cengel815@gmail.com>
2019-08-17 16:08:05 -0500

    Scope Functions
    Code from the scope functions video tutorial.

---
+++ src/main/kotlin/ScopeFunctions.kt
@@ -0,0 +1,48 @@
+fun main() {
+    println("Scope Functions")
+    println()
+    val scopeFunctions = "Hello scope functions!"
+
+    val let = scopeFunctions.let {
+        println("What is let?")
+        it.replace(" ", ".").reversed()
+    }
+    println(let)
+    println()
+
+    val run = scopeFunctions.run {
+        println("What is run?")
+        toUpperCase().removePrefix("HELLO").trim()
+    }
+    println(run)
+    println()
+
+    val with = with(scopeFunctions) {
+        println("What is with?")
+        drop(5).toByteArray().contentToString()
+    }
+    println(with)
+    println()
+
+    val apply = mutableListOf<Int>().apply {
+        println("What is apply?")
+        add(1)
+        add(2)
+        add(3)
+    }
+    println(apply.toString())
+    println()
+
+//    val withOutApply = mutableListOf<Int>()
+//    withOutApply.add(1)
+//    withOutApply.add(2)
+//    withOutApply.add(3)
+
+    val also = mutableListOf<Int>().also {
+        it.add(1)
+        it.add(2)
+        println("This is a side effect thing.")
+    }
+    println(also.toString())
+    println()
+}
