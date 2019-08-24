ae1e391d42ccc652842efefa306ea4b624e0f1bf
Cody ENgel <cengel815@gmail.com>
2019-08-24 13:54:21 -0500

    Override Rules
    Added examples for overriding with classes.

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -2,11 +2,11 @@ import kotlin.random.Random
 
 fun main() {
     printXtraLn("Properties & Fields / Calculator")
-    val calculator = RandomCalculator()
-    calculator.add(12)
-    calculator.add(32)
+    val calculator = BadCalculator()
+    calculator.add(2)
+    calculator.add(2)
     calculator.subtract(3)
-    calculator.multiply(54)
+    calculator.multiply(4)
     calculator.add(1)
     printXtraLn("Result: ${calculator.currentValue}")
 //    val remainder = calculator.remainder(2)
@@ -15,17 +15,19 @@ fun main() {
 
 class RandomCalculator : Calculator(
     initialValue = Random.nextInt(Int.MIN_VALUE, Int.MAX_VALUE)
-) {
-    override fun print(string: String) {
-        println("$string - ALBLJADFLJ")
-    }
+)
+
+class BadCalculator : Calculator() {
+    override var currentValue: Int = super.currentValue
+        get() {
+            return field - 1
+        }
+        set(value) {
+            field = value * value
+        }
 }
 
 class BetterCalculator(initialValue: Int = 0) : Calculator(initialValue) {
-    override fun print(string: String) {
-        println(string)
-    }
-
     fun remainder(number: Int): Int {
         println("$currentValue % $number")
         return currentValue % number
@@ -33,12 +35,10 @@ class BetterCalculator(initialValue: Int = 0) : Calculator(initialValue) {
 }
 
 abstract class Calculator(initialValue: Int = 0) {
-    var currentValue = initialValue
-
-    abstract fun print(string: String)
+    open var currentValue = initialValue
 
     fun add(number: Int): Int {
-        print("$currentValue + $number")
+        println("$currentValue + $number")
         currentValue += number
         return currentValue
     }
