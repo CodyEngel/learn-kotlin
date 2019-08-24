833991f631a0bb49eb4c68b410df6190d341d7b7
Cody ENgel <cengel815@gmail.com>
2019-08-24 11:02:21 -0500

    Abstract Classes
    Talked about abstract classes and all the fun associated with them.

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,20 +1,44 @@
+import kotlin.random.Random
+
 fun main() {
     printXtraLn("Properties & Fields / Calculator")
-    val calculator = Calculator(initialValue = 2)
+    val calculator = RandomCalculator()
     calculator.add(12)
     calculator.add(32)
     calculator.subtract(3)
     calculator.multiply(54)
     calculator.add(1)
-    calculator.divide(2)
     printXtraLn("Result: ${calculator.currentValue}")
+//    val remainder = calculator.remainder(2)
+//    printXtraLn("Remainder: $remainder")
 }
 
-class Calculator(initialValue: Int = 0) {
+class RandomCalculator : Calculator(
+    initialValue = Random.nextInt(Int.MIN_VALUE, Int.MAX_VALUE)
+) {
+    override fun print(string: String) {
+        println("$string - ALBLJADFLJ")
+    }
+}
+
+class BetterCalculator(initialValue: Int = 0) : Calculator(initialValue) {
+    override fun print(string: String) {
+        println(string)
+    }
+
+    fun remainder(number: Int): Int {
+        println("$currentValue % $number")
+        return currentValue % number
+    }
+}
+
+abstract class Calculator(initialValue: Int = 0) {
     var currentValue = initialValue
 
+    abstract fun print(string: String)
+
     fun add(number: Int): Int {
-        println("$currentValue + $number")
+        print("$currentValue + $number")
         currentValue += number
         return currentValue
     }
